4f46e65bb5aa4bcff2c86d5d54ff1a7d072a7e1e
tomoyk <tmyk.kym@gmail.com>
2018-01-13 00:35:42 +0900

    fixed getThumb.bash

--- getThumb.bash
+++ getThumb.bash
@@ -1,17 +1,41 @@
-#!/bin/bash -eu
+#!/bin/bash -e
 
 # FILENAME="Eject.mp4"
 # IMAGENAME="out2.jpg"
 FILEPATH="$PWD"
+GETPOINT="00:00:10"
 
+##
+# Check parameters
+##
 if [ -v "$1" ] || [ -v "$2" ] ; then
-  echo "Usage: CMD [SourceVideo] [ThumbnailFile]"
+  echo "Usage: CMD <SourceVideo> <ThumbnailFile> [getTime]"
   exit 1
-else
-  FILENAME="$1"
-  IMAGENAME="$2"
 fi
 
-sudo docker run --rm -v ${FILEPATH}:/tmp jrottenberg/ffmpeg -stats -i /tmp/${FILENAME} -y -vframes 1 -f image2 -ss 00:00:05 /tmp/${IMAGENAME}
+if [ ! -v $3 ] ; then
+  GETPOINT="$3"
+fi
+
+##
+# Does set file exist ?
+##
+if [ -f "$1" ] ; then
+  FILENAME="$1"
+  IMAGENAME="$2"
+else
+  echo "No such file ($1)"
+  exit 1
+fi
+
+sudo docker run \ 
+  --rm \
+  -v ${FILEPATH}:/tmp \
+  jrottenberg/ffmpeg \
+  -ss ${GETPOINT} \
+  -i /tmp/${FILENAME} \
+  -y -vframes 1 \
+  -f image2 \
+  /tmp/${IMAGENAME}
 
 exit 0
