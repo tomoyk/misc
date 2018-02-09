a309120512108cbfd9e03d410fc15d4c4c49938a
tomoyk <tmyk.kym@gmail.com>
2018-02-09 10:56:01 +0900

    fixed simpleHttpServer.py

--- simpleHttpServer.py
+++ simpleHttpServer.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python
-import http.server as web
+import http.server as web 
+import socketserver
 
-bindAddress = ("127.0.0.1", 3000)
-requestHandler = web.BaseHTTPRequestHandler(request, client_address, server)
+def run(handler_class=web.BaseHTTPRequestHandler):
+  bindAddr = ("0.0.0.0", 3000)
+  req = web.BaseHTTPRequestHandler;
+  httpd = web.HTTPServer(bindAddr, req)
+  print("=====")
+  print("IP-Address: "+str(bindAddr[0]))
+  print("Port: "+str(bindAddr[1]))
+  print("=====")
+  httpd.serve_forever()
 
-httpd = web.HTTPServer(bindAddress, requestHandler)
-httpd.serve_forever()
+def run2():
+  PORT = 3000
+  HOST = "0.0.0.0"
+  Handler = web.SimpleHTTPRequestHandler
+
+  with socketserver.TCPServer((HOST, PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
+
+run2()
+
